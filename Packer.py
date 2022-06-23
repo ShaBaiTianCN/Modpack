eb02184ce772028e4dfad2e90a9876c7b65cafd2
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-06-23 14:30:03 +0800

    ci: 👷 refactor ci

--- Packer.py
+++ Packer.py
@@ -1,18 +1,21 @@
 import os
 import json
 import shutil
-import zipfile
 
 import requests
 
-with open('ReleaseConfig.json', encoding='utf-8') as f:
-    RELEASE_CONFIG = json.load(f)
-    PROJECT_NAME = RELEASE_CONFIG['projectName']
-    PACK_VERSION = RELEASE_CONFIG['packVersion']
+with open('PackerConfig.json', encoding='utf-8') as f:
+    PACKER_CONFIG = json.load(f)
+    PROJECT_NAME = PACKER_CONFIG['projectName']
+    PACK_VERSION = PACKER_CONFIG['packVersion']
 
-VERSION = os.environ['version'] if 'version' in os.environ.keys() else input()
+if os.environ['event_name'] == 'pull_request_target':
+    VERSION = os.environ['event_number']
+elif os.environ['event_name'] == 'workflow_dispatch':
+    VERSION = os.environ['version']
+else:
+    VERSION = input('Version: ')
 RELEASE_NAME = '-'.join([VERSION, 'CN', PROJECT_NAME, PACK_VERSION])
-RELEASE_DIR = os.path.join('temp', RELEASE_NAME)
 
 RESOURCES_PACK_URL = 'https://api.github.com/repos/ShaBaiTianCN/Minecraft-Mod-Language-Package-Not-Included-Language-Package/releases/latest'
 
@@ -28,11 +31,11 @@ def touch_dir(path: str):
 
 def copy(file_path: str, check_dir: bool = True):
     """
-    Copy file to release folder.
+    Copy file to target folder.
     :param file_path: File path.
     :param check_dir: Enable check dir exist.
     """
-    target_path = os.path.join(RELEASE_DIR, file_path)
+    target_path = os.path.join(RELEASE_NAME, file_path)
     if check_dir:
         touch_dir(os.path.dirname(target_path))
     shutil.copyfile(file_path, target_path)
@@ -40,10 +43,10 @@ def copy(file_path: str, check_dir: bool = True):
 
 def main():
     # Touch folder
-    touch_dir(RELEASE_DIR)
+    touch_dir(RELEASE_NAME)
 
     # Copy file
-    for i in RELEASE_CONFIG['files']:
+    for i in PACKER_CONFIG['files']:
         if os.path.isfile(i):
             copy(i)
         else:
@@ -53,30 +56,13 @@ def main():
 
     # Download resource pack
     release_info = requests.get(RESOURCES_PACK_URL).json()['assets'][0]
-    resourcepack_dir = os.path.join(RELEASE_DIR, 'resourcepacks')
-    resourcepack_path = os.path.join(resourcepack_dir, '汉化补充资源包.zip')
-    touch_dir(resourcepack_dir)
-    with open(resourcepack_path, 'wb') as f:
+    resourcepacks_dir = os.path.join(RELEASE_NAME, 'resourcepacks')
+    resourcepacks_path = os.path.join(resourcepacks_dir, '汉化补充资源包.zip')
+    touch_dir(resourcepacks_dir)
+    with open(resourcepacks_path, 'wb') as f:
         f.write(requests.get(release_info['browser_download_url']).content)
 
-    # Make Zipfile
-    with zipfile.ZipFile(
-            f'{RELEASE_DIR}.zip',
-            mode='w',
-            compression=zipfile.ZIP_DEFLATED
-    ) as archive:
-        for dirpath, dirnames, filenames in os.walk(RELEASE_DIR):
-            for filename in filenames:
-                file_path = os.path.join(dirpath, filename)
-                archive.write(
-                    file_path,
-                    arcname=file_path.replace(RELEASE_DIR, '')
-                )
-
-    # Remove folder
-    shutil.rmtree(RELEASE_DIR)
-
-    # Release Info
+    # Set release name
     print(f'::set-output name=release_name::{RELEASE_NAME}')
 
 
